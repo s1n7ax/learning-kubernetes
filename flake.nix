6717eb5a67577250d5e11163caeae8acf9fb9a6f
s1n7ax <srineshnisala@gmail.com>
2024-09-30 15:40:39 +0530

    chore: init project

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "devShell for learning kubernetes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            kubectl
+            minikube
+            eza
+            fd
+          ];
+
+          shellHook = ''
+            alias ls=eza -lg
+            alias find=fd
+          '';
+        };
+      }
+    );
+}
